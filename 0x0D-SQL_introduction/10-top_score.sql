74a39827a6ae5ee581915777f357f4b3954be801
Lesley366 <lesleytalana@gmail.com>
2022-11-01 11:12:45 -0700

    task 10

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,6 +1,6 @@
 -- lists all records of the table second_table of the database
 -- in my MySQL server.
 
-SELECT 'score', 'name'
-FROM 'second_table'
-ORDER BY 'score' DESC;
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
